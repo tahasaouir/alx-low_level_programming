9a7dc4ce93f1cf54d0ab2b19e9ea127c804aed75
tahasaouir <tahasr2014@gmail.com>
2023-02-16 15:48:46 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * return: 0 (Success)
+ */
 int main(void) {
 	puts("\"Programming is like building a multilingual
 		       	puzzle");
