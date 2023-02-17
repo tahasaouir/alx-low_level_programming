fbfc849b72656ec30007824916f8a6563617f368
tahasaouir <tahasr2014@gmail.com>
2023-02-16 19:02:55 -0800

    1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Prints the size of various types based on the computer it is compiled and run on 
+ * main - A program that prints the size of various computer types
  * Return: Always 0.
  */
-int main (void)
+int main(void)
 {
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
