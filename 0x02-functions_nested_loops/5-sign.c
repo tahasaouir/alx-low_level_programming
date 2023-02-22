750aac696f19065ba1e5a344964314d39791b74a
tahasaouir <tahasr2014@gmail.com>
2023-02-22 14:34:39 -0800

    updated

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,6 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
-
 int print_sign(int n)
 {
 	if (n > 0)
@@ -18,7 +17,8 @@ int print_sign(int n)
 }
 		_putchar(48);
 		return (0);
-	} else if (n < 0)
+	} 
+          else if (n < 0)
 	{
 		_putchar('-');
 	}
